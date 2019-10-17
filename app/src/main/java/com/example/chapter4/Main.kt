6d1b8d9d89a9810c6f6cf1da7b5139b0b99d55c8
nongter <thanasit.wi@rmuti.ac.th>
2019-10-17 19:23:51 +0700

    video4.7.1

--- app/src/main/java/com/example/chapter4/Main.kt
+++ app/src/main/java/com/example/chapter4/Main.kt
@@ -2,6 +2,7 @@ fun  main(args:Array<String>){
     println("Hello World")
     println("My first Kotlin program")
 
+    var jim:String
     var  tim :String
     tim="Tim Buchalka"
     println(tim)
@@ -16,15 +17,17 @@ fun  main(args:Array<String>){
     val  apples:Int=6
     val  orenges :Int=5
     var fruit :Int= apples-orenges
-    println(fruit)
+    println("$apples apples - $orenges oranges leaves $fruit piece(s) of fruit ")
 
-    println(apples/4)
+    println("A quarter of the apples is ${apples/4}")
 
     println()
 
     val  weeks:Int=234
     val  years:Double=weeks/52.0
     println("$weeks weeks is $years years")
+
+    tim="James T Kirk"
 
     println("My name is"+tim)
     println("I can print \$tim ")
