90eb8dbf033326faba9ec2f7f768f0c1d364da87
nongter <thanasit.wi@rmuti.ac.th>
2019-10-17 20:10:54 +0700

    video4.11

--- app/src/main/java/com/example/chapter4/Main.kt
+++ app/src/main/java/com/example/chapter4/Main.kt
@@ -87,8 +87,13 @@ fun  main(args: Array<String>, one2watch: Any) {
     println(gr8.weapon.damageInflicted)
 
     tim.weapon = Weapon("Sword",10)
-    println(tim.name)
+   // println(tim.name)
+    tim.show()
+
+    louise.weapon=tim.weapon
+    louise.show()
 
    tim.weapon= Weapon("Spear",14)
-    println(tim.weapon.name)
+    //println(tim.weapon.name)
+    tim.show()
 }
--- app/src/main/java/com/example/chapter4/Player.kt
+++ app/src/main/java/com/example/chapter4/Player.kt
@@ -11,6 +11,8 @@ class Player (val name:String,var level:Int=1,var lives:Int=3,var score:Int=0){
             lives:$lives
             level:$level
             score:$score
+            weapon:${weapon.name}
+            damag:${weapon.damageInflicted}
             """)
     }
 }
