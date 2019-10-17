91585411c102b20dcb96e366fb5c6fa5f6ed2eda
nongter <thanasit.wi@rmuti.ac.th>
2019-10-17 19:08:05 +0700

    video4.5.1

--- app/src/main/java/com/example/chapter4/Main.kt
+++ app/src/main/java/com/example/chapter4/Main.kt
@@ -1,12 +1,13 @@
 fun  main(args:Array<String>){
     println("Hello World")
     println("My first Kotlin program")
+
     var  tim :String
     tim="Tim Buchalka"
     println(tim)
 
-    var  timSalary:Int =32
-    var  monthly:Int=timSalary*4
-    println(timSalary)
-    println(monthly)
+    var  timsWeeklySalary:Int =32
+    var  timMonthlySalary:Int=timsWeeklySalary*4
+    println(timsWeeklySalary)
+    println(timMonthlySalary)
 }
